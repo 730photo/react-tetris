d3f2c08f2410fef73029b0ccddb405e2e41c7adb
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2020-04-21 12:41:15 -0400

    everything is set up nicely

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 
 //we have backticks because this is a tagged function. You can write regular CSS inside of it
 export const StyledCell = styled.div`
-    width: auto;
-    background: rgba(${props => props.color}, 0.8);
-    border: ${props => (props.type === 0 ? '0px solid' : '4px sold')};
-    border-bottom-color: rgba(${props => props.color}, 0.1);
-    border-right-color: rgba(${props => props.color}, 1);
-    border-top-color: rgba(${props => props.color}, 1);
-    border-left-color: rgba(${props => props.color}, 0.3);
+  width: auto;
+  /* gives the stage a box-shadow effect */
+  background: rgba(${props => props.color}, 0.8);
+  border: ${props => (props.type === 0 ? '0px solid' : '4px solid')};
+  border-bottom-color: rgba(${props => props.color}, 0.1);
+  border-right-color: rgba(${props => props.color}, 1);
+  border-top-color: rgba(${props => props.color}, 1);
+  border-left-color: rgba(${props => props.color}, 0.3);
     `
