f5c99c4bb8c0d64d115df4137abbeb74a47b2e8f
architekespy <architekespy@Reginalds-MacBook-Pro.local>
2020-05-11 11:17:27 -0400

    finally finished👍💪🏾

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+import { createStage, checkCollision } from '../gameHelpers';
 
 // Styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 // Components
 import Stage from './Stage';
@@ -17,30 +19,67 @@ import StartButton from './StartButton';
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
-
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared);
 
   console.log('re-render');
 
 // takes care of the left and the right movement
-  const movePlayer = dir => {
-    updatePlayerPos({ x: dir, y: 0 })
-  }
+const movePlayer = dir => {
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
+  };
 
   const startGame = () => {
     // Reset everything
     setStage(createStage());
+    setDropTime(1000);
     resetPlayer();
+    setGameOver(false);
+    setScore(0);
+    setRows(0);
+    setLevel(0);
   }
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false })
-}
-  
-const dropPlayer = () => {
-    drop();
-}
+    // increase level once player has cleared 10 rows
+    if (rows > (level + 1) * 10) {
+      setLevel(prev => prev +1);
+      // Also increase speed
+      setDropTime (1000 / (level + 1) + 200);
+    }
+
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+        updatePlayerPos({ x: 0, y: 1, collided: false });
+      } else {
+        // Game over!
+        if (player.pos.y < 1) {
+            console.log('GAME OVER!!!');
+            setGameOver(true);
+            setDropTime(null);
+          }
+          updatePlayerPos({ x: 0, y: 0, collided: true });
+        }
+      };
+
+      const keyUp = ({ keyCode }) => {
+        if (!gameOver) {
+          if (keyCode === 40) 
+          console.log("interval on")
+          setDropTime(1000 / (level + 1) + 200);
+        }
+      }
+    
+      const dropPlayer = () => {
+        console.log("interval off")
+        // We don't need to run the interval when we use the arrow down to
+        // move the tetromino downwards. So deactivate it for now.
+        setDropTime(null);
+        drop();
+      };
 
 // callback function when we press the keys on the keyboard
 const move  = ({ keyCode }) => {
@@ -54,12 +93,22 @@ const move  = ({ keyCode }) => {
             // moves player down
         } else if (keyCode === 40) {
             dropPlayer();
+        } else if (keyCode === 38) {
+            playerRotate(stage, 1);
         }
     }
-}
+};
+
+useInterval(() => {
+  drop();
+}, dropTime)
 
   return (
-    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
+    <StyledTetrisWrapper 
+    role="button" 
+    tabIndex="0" 
+    onKeyDown={e => move(e)} onKeyUp={keyUp}
+    >
       <StyledTetris>
             {/* Tetris stage */}
             <Stage stage ={stage} />
@@ -70,9 +119,9 @@ const move  = ({ keyCode }) => {
                 ) : (
                 <div>
                     {/* if the game is not over */}
-                <Display text="Score"/>
-                <Display text="Rows"/>
-                <Display text="Level"/>
+                <Display text={`Score: ${score}`} />
+                <Display text={`Rows: ${rows}`} />
+                <Display text={`Level: ${level}`} />
                 </div>
                 )}
                 {/* resets the stage and resets the player */}
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,33 +1,60 @@
 import { useState, useCallback } from 'react';
 
 import { TETROMINOS, randomTetromino } from '../tetrominos';
-import { STAGE_WIDTH } from '../gameHelpers';
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
         // position for the player
-        pos: { x: 0, y: 0 },
-        tetromino: TETROMINOS[0].shape,
-        collided: false,
+    pos: { x: 0, y: 0 },
+    tetromino: TETROMINOS[0].shape,
+    collided: false,
   });
 
 
-  const updatePlayerPos = ({ x, y, collided }) => {
-    setPlayer(prev => ({
-      ...prev,
-      pos: { x: (prev.pos.x += x), y: (prev.pos.y += y )},
-      collided,
-    }))
+  function rotate(matrix, dir) {
+    // Make the rows to become cols (transpose)
+    const mtrx = matrix.map((_, index) => matrix.map(column => column[index]));
+    // Reverse each row to get a rotaded matrix
+    if (dir > 0) return mtrx.map(row => row.reverse());
+    return mtrx.reverse();
   }
+
+// if we collide with something whiole rotating it prevents us from going through it
+function playerRotate(stage, dir) {
+  const clonedPlayer = JSON.parse(JSON.stringify(player));
+  clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+  const pos = clonedPlayer.pos.x;
+  let offset = 1;
+  while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+    clonedPlayer.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > clonedPlayer.tetromino[0].length) {
+      rotate(clonedPlayer.tetromino, -dir);
+      clonedPlayer.pos.x = pos;
+      return;
+    }
+  }
+  setPlayer(clonedPlayer);
+}
+
+const updatePlayerPos = ({ x, y, collided }) => {
+  setPlayer(prev => ({
+    ...prev,
+    pos: { x: (prev.pos.x += x), y: (prev.pos.y += y) },
+    collided,
+  }));
+};
 
   // useCallback prevents infinite loop
   const resetPlayer = useCallback(() => {
-      setPlayer({
-        pos: { x: STAGE_WIDTH /2 -2, y: 0},
-        tetromino: randomTetromino().shape,
-        collided: false,
-      })
-    },[])
-  
-  return [player, updatePlayerPos, resetPlayer];
-}
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+      tetromino: randomTetromino().shape,
+      collided: false,
+    });
+  }, []);
+
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
+};
